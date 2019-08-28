9d352d0a10891d2b3862ed83129e6da0eb105740
Aayyi <julien.lavocat@gmail.com>
2019-08-28 17:10:39 +0200

    Added shuffled propositions method

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -6,6 +6,7 @@ export class Question {
 	readonly question: string;
 	readonly correctAnswer: string;
 	readonly incorrectAnswers: string[];
+	readonly propositions: string[];
 
 	constructor(obj: any) {
 		this.category = obj.category;
@@ -14,13 +15,21 @@ export class Question {
 		this.question = obj.question;
 		this.correctAnswer = obj.correct_answer;
 		this.incorrectAnswers = obj.incorrect_answers;
+		this.propositions = shuffle([...this.incorrectAnswers, this.correctAnswer]);
 	}
 
 
 	isAnswerCorrect(answer: string) {
 		return answer && this.correctAnswer === answer;
 	}
+}
 
+function shuffle(a: any[]) {
+	for (let i = a.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[a[i], a[j]] = [a[j], a[i]];
+	}
+	return a;
 }
 
 export type QuestionType = "multiple" | "boolean";
